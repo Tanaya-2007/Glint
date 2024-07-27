91a99c1ffe79f1268a917a40b098027330dc3e28
Tanaya Pawar <tanayapawar02@gmail.com>
2024-07-27 18:56:15 +0530

    Added GSAP

--- script.js
+++ script.js
@@ -10,38 +10,107 @@
 //         clearInterval(cnt);
 //     }   
 // } ,10)
-// gsap.from(".logo img",{
-//     y:-100,
-//     duration:2
-// })
 
 Shery.mouseFollower({
-    //Parameters are optional.
-    skew: true,
-    width:"1%",
-    height:"1%",
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
-  Shery.makeMagnet(".logo img" /* Element to target.*/, {
-    //Parameters are optional.
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
-const card = document.querySelector(".logo img");
-
-card.addEventListener("mousemove", (e) => {
-  let x = e.offsetX;
-  let y = e.offsetY;
-  
-  gsap.to(card, {
-   
-  });
-});
-card.addEventListener("mouseleave", (e) => {
-  gsap.to(card, {
-    x: 0,
-    y: 0,
-  });
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
 });
 
+Shery.makeMagnet(".logo img,h1", {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+gsap.from(".main-head h1,.main-head h3",{
+     y:250,
+     duration:1.5,
+     opacity:0
+}
+)
+gsap.from(".btn",{
+  scale:0.2,
+  opacity:0,
+  duration:1,
+  delay:2
+})
+
+gsap.from("#home .icon",{
+   x:150,
+   stagger:0.3
+})
+
+gsap.from(".about-info h1",{
+    y:-100,
+    opacity:0,
+    scrollTrigger:{
+      trigger:"body",
+      start:"20%",
+      end:"30%"
+    }
+})
+function breakText() {
+    var h1 = document.querySelector(".about-info p");
+    var h1Text = h1.textContent;
+    
+    var splitText = h1Text.split(/\s+/);
+
+    var cut = "";
+    splitText.forEach(function (elem) {
+      cut += `<span>${elem}&nbsp;</span>`;
+      })
+      h1.innerHTML = cut;
+}  
+
+  breakText();
+
+gsap.from("p span",{
+  y:70,
+  opacity:0,
+  duration:0.9,
+  stagger:0.1,
+  scrollTrigger:{
+    trigger:"body",
+    start:"15%",
+    end:"35%",
+    scrub:2
+  }
+})
+
+gsap.from(".counters",{
+  y:200,
+  scale:0.2,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:"body",
+    start:"35%"
+  }
+})
+
+gsap.from(".service h1",{
+  y:-100,
+  opacity:0,
+  scrollTrigger:{
+    trigger:"body",
+    start:"50%"
+  }
+})
+
+gsap.from(".service-info",{
+  x:-300,
+  y:-300,
+  scale:0.2,
+  stagger:0.6,
+  opacity:0,
+  scrub:1,
+  duration:1,
+  scrollTrigger:{
+    trigger:"body",
+    start:"60%",
+    end:"90%",
+    markers:true
+  }
+})
