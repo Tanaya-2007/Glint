101138916fdc7ff30acedb5cbf0432d447333a15
Tanaya Pawar <tanayapawar02@gmail.com>
2024-07-27 19:00:53 +0530

    GSAP

--- script.js
+++ script.js
@@ -12,14 +12,12 @@
 // } ,10)
 
 Shery.mouseFollower({
-  //Parameters are optional.
   skew: true,
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
 
 Shery.makeMagnet(".logo img,h1", {
-  //Parameters are optional.
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
